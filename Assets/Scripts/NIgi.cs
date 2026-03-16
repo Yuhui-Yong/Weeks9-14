1a5e103dccc27d309583b8d4ebf1c2d7cfa88671
Yuhui Yong <yongy@shernet.sheridancollege.ca>
2026-03-16 15:02:47 -0400

    I forgot to comment things because I think I need this specifically to be commented since I understood fully and it was interesting and something like that.

--- Assets/Scripts/NIgi.cs
+++ Assets/Scripts/NIgi.cs
@@ -4,9 +4,9 @@ using UnityEngine.UIElements;
 
 public class NIgi : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
-    public Sprite[] TheHero;
-    int index = 0;
+    public SpriteRenderer spriteRenderer; // I am calling the sprite in the inspector.
+    public Sprite[] TheHero; // I am making it have an array of the name TheHero.
+    int index = 0; // The current's sprite or the current value I am using.
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,13 +19,14 @@ public class NIgi : MonoBehaviour
     {
         Debug.Log("index");
 
-        if (index == TheHero.Length - 1)
+        if (index == TheHero.Length - 1) // I cannot just set to TheHero.Length because it cannot detect the elements I have since it starts from the zero, which means
+            // it goes from 0, 1, 2, 3 and 4 but there is no 5, so I had to subtract by 1.
         {
-            index = 0;
+            index = 0; // So, if index is equal to zero, it will go to spriteRenderer.sprite = TheHero[index];, which is the original sprite, like the object that I have.
         }
         else
         {
-            index++;
+            index++; // Otherwise, it will keep running like adding one, so it will like animate it.
         }
 
         spriteRenderer.sprite = TheHero[index];
