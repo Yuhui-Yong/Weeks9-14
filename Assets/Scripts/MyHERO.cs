7d9b3ae800519e1b2eaea239a219b25537f8b90a
Yuhui Yong <yongy@shernet.sheridancollege.ca>
2026-03-20 17:32:35 -0400

    Coding GYM I have done for 30 mins, mostly figuring out coroutine thing but I did it too short So, I will have to do it more.

---
+++ Assets/Scripts/MyHERO.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class MyHERO : MonoBehaviour
+{
+    public AnimationCurve Anime;
+    private Coroutine MoveMyhero;
+    public float duration;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator MyheroMoveUpdate()
+    {
+        float progress = 0f;
+
+        // The contents of the while loop run while the condition is true
+        while (progress < duration)
+        {
+            progress += Time.deltaTime;
+            transform.localPosition = Anime.Evaluate(progress / duration) * Vector3.one;
+        }
+        yield return null;
+    }
